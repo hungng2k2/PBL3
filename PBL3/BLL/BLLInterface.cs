cffb7fcf7044607d428b4dca27d99e269b27b5c8
HungNG2k2 <hung200220@gmail.com>
2022-04-30 16:45:28 +0700

    add bll

---
+++ PBL3/BLL/BLLInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBL3.BLL
+{
+    interface BLLInterface<T>
+    {
+        List<T> GetAll();
+
+        void checkAddUpdate(string id);
+
+        void ExecuteAddUpdate(T t);
+
+        void Delete(string id);
+    }
+}
---
+++ PBL3/BLL/BLLNhanVien.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PBL3.DTO;
+using PBL3.DAL;
+
+namespace PBL3.BLL
+{
+    class BLLNhanVien : BLLInterface<NhanVien>
+    {
+        private static BLLNhanVien _Instance;
+
+        public static BLLNhanVien Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new BLLNhanVien();
+                }
+                return _Instance;
+            }
+            private set { }
+        }
+
+        private BLLNhanVien()
+        {
+
+        }
+
+        public void checkAddUpdate(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteAddUpdate(NhanVien t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<NhanVien> GetAll()
+        {
+            return DALNhanVien.Instance.GetAll();
+        }
+    }
+}
